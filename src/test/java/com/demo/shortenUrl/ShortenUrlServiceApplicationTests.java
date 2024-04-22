ab462f410ff6444eacaa73eca4021a8818a2197e
Randy Ananda <randy.aaananda@gmail.com>
2024-04-21 23:07:53 -0700

    add test cases

--- src/test/java/com/demo/shortenUrl/ShortenUrlServiceApplicationTests.java
+++ src/test/java/com/demo/shortenUrl/ShortenUrlServiceApplicationTests.java
@@ -1,9 +1,12 @@
 package com.demo.shortenUrl;
 
+import static org.hamcrest.Matchers.startsWith;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -46,4 +49,66 @@ public class ShortenUrlServiceApplicationTests {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.shortUrl").exists());
     }
+    
+    @Test
+    public void givenFullUrlReturnJsonWithShortUrlValueHasHttp() throws Exception {
+        String fullUrl = "https://example.com/foo";
+        mockmvc = MockMvcBuilders.webAppContextSetup(context).build();
+        mockmvc.perform(post("/api/encode")
+                .contentType(MediaType.APPLICATION_JSON)
+                .param("fullUrl", fullUrl))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.shortUrl", startsWith("http")));
+    }
+    
+    @Test
+    public void shouldNotInsertFullUrlIfAlreadyExists() throws Exception {
+        String fullUrl = "https://example.com/foo";
+        mockmvc = MockMvcBuilders.webAppContextSetup(context).build();
+        String shortUrl1 = mockmvc.perform(post("/api/encode")
+                .contentType(MediaType.APPLICATION_JSON)
+                .param("fullUrl", fullUrl))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.shortUrl", startsWith("http"))).andReturn().getResponse().getContentAsString();
+
+        String shortUrl2 = mockmvc.perform(post("/api/encode")
+                .contentType(MediaType.APPLICATION_JSON)
+                .param("fullUrl", fullUrl))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.shortUrl", startsWith("http"))).andReturn().getResponse().getContentAsString();
+
+        Assert.assertEquals(shortUrl1, shortUrl2);
+    }
+    
+    @Test
+    public void shouldNotInsertFullUrlIfDoesNotExist() throws Exception {
+        String fullUrl1 = "https://example.com/foo1";
+        String fullUrl2 = "https://example.com/foo2";
+        mockmvc = MockMvcBuilders.webAppContextSetup(context).build();
+        
+        String shortUrl1 = mockmvc.perform(post("/api/encode")
+                .contentType(MediaType.APPLICATION_JSON)
+                .param("fullUrl", fullUrl1))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.shortUrl", startsWith("http"))).andReturn().getResponse().getContentAsString();
+
+        String shortUrl2 = mockmvc.perform(post("/api/encode")
+                .contentType(MediaType.APPLICATION_JSON)
+                .param("fullUrl", fullUrl2))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.shortUrl", startsWith("http"))).andReturn().getResponse().getContentAsString();
+
+        Assert.assertNotEquals(shortUrl1, shortUrl2);
+    }
+    
+    @Test
+    public void givenShortUrlNotFoundInDatabase() throws Exception {
+        String shortUrl = "http://short/x";
+        
+    	mockmvc = MockMvcBuilders.webAppContextSetup(context).build();
+        mockmvc.perform(get("/api/decode")
+                .contentType(MediaType.APPLICATION_JSON)
+                .param("shortUrl", shortUrl))
+                .andExpect(status().is5xxServerError());
+    }	
 }
