6087e22669fc7ed747846e4b17862f29130f0500
Randy Ananda <randy.aaananda@gmail.com>
2024-04-21 22:18:28 -0700

    add test case

--- src/test/java/com/demo/shortenUrl/ShortenUrlServiceApplicationTests.java
+++ src/test/java/com/demo/shortenUrl/ShortenUrlServiceApplicationTests.java
@@ -1,13 +1,63 @@
 package com.demo.shortenUrl;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class ShortenUrlServiceApplicationTests {
+@AutoConfigureMockMvc
+public class ShortenUrlServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
+	
+    private MockMvc mockmvc;
+    
+    @Autowired
+    private WebApplicationContext context;
+    
+    ObjectMapper om = new ObjectMapper();
+    
+    @Test
+    public void setup() {
+    	mockmvc = MockMvcBuilders.webAppContextSetup(context).build();
+    	System.out.println("mockmvc: " + mockmvc);
+    }
+    
+    @Test
+    public void sampleTest() throws Exception {
+    	System.out.println("Testing Randy: ");
+    	Assert.assertEquals(0, 0);
+    }
+    
+    @Test
+    public void givenFullUrlReturnStatusOk() throws Exception {
+        String fullUrl = "https://example.com/foo";
+        
+        String jsonRequest = om.writeValueAsString(fullUrl);
+    	mockmvc = MockMvcBuilders.webAppContextSetup(context).build();
+    	System.out.println("mockmvc1: " + mockmvc);
+        MvcResult res = mockmvc.perform(post("/api/encode")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonRequest))
+                .andExpect(status().isOk())
+                .andReturn();
+        System.out.println("MvcResult: " + res);
+    }
 }
